0d19a59b06527a71b059c965ffb4922a3b2a110b
Abhishek Vedant <83869760+Webby1015@users.noreply.github.com>
2023-02-15 17:24:12 +0530

    random palettes are generating now

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
+const container = document.querySelector(".container")
 const refresh = document.querySelector(" .refresh");
 const maxPaletteBoxes = 32;
 const generatePalette = ()=>{
     for(let i =0;i<maxPaletteBoxes;i++){
         let randomHex = Math.floor(Math.random()* 0xffffff).toString(16);
-        randomHex = '#'+randomHex;
+        randomHex = `#${randomHex.padStart(6,"0")}`;
         console.log(randomHex)
-    }
         const color =document.createElement("li");
         color.classList.add("color");
+        color.innerHTML=`<div class="rect-box" style="background:${randomHex};  "></div>
+        <span class="hex-value">${randomHex}</span>`
+        container.appendChild(color);
+    }
 }
 refresh.addEventListener("click",generatePalette);
